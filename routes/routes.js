db74aea50b1afc9e55624ef54540d863f756d1be
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-20 19:20:46 -0300

    Implementa as rotas para persistir no MongoDB

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const transactionRouter = express.Router();
+const transactionService = require('../services/transactionService');
+
+transactionRouter.get('/', transactionService.find);
+transactionRouter.get(
+  'periods/distinct',
+  transactionService.findDistincPeriods
+);
+transactionRouter.post('/', transactionService.create);
+transactionRouter.put('/', transactionService.update);
+transactionRouter.delete('/', transactionService.remove);
 
 module.exports = transactionRouter;
