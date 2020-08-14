f6a7c58b9a4e49fd7795386d9e7a3d82d6061307
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-08-14 12:36:14 -0300

    Atualização de código back-end

--- routes/routes.js
+++ routes/routes.js
@@ -1,103 +1,151 @@
 const express = require('express');
 const transactionRouter = express.Router();
 const transactionService = require('../services/transactionService');
-//const dateHelpers = require('../helpers/dateHelpers');
 
-/* transactionRouter.get('/', async (req, res) => {
+transactionRouter.get('/', async (req, res) => {
   const { query } = req;
 
   try {
     if (!query.period) {
-      throw new Error('Parâmetro "period" em formato AAAA-MM');
+      throw new Error(
+        'Parâmetro "period" incorreto, tem que ser no formato yyyy-mm'
+      );
     }
+
     const { period } = query;
-    dateHelpers.validatePeriod(period);
 
-    const filterTrasactions = await transactionService.find(period);
+    if (period.length !== 7) {
+      throw new Error('Período inválido. Use o formato yyyy-mm');
+    }
+
+    const filteredTransactions = await transactionService.getTransactionsFrom(
+      period
+    );
+
     res.send({
-      length: filterTrasactions.length,
-      transactions: filterTrasactions,
+      length: filteredTransactions.length,
+      transactions: filteredTransactions,
     });
-  } catch ({ mess }) {
-    console.log(mess);
-    res.status(400).send({ error: mess });
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
   }
-}); */
+});
 
-transactionRouter.get('/', transactionService.find);
-transactionRouter.get(
-  'periods/distinct',
-  transactionService.findDistincPeriods
-);
 transactionRouter.post('/', async (req, res) => {
   const { body } = req;
 
   try {
-    await validateTransactionData(body);
+    if (JSON.stringify(body) === '{}') {
+      throw new Error('Conteúdo inexistente');
+    }
 
     const { description, value, category, year, month, day, type } = body;
-    const period = dateHelpers.createPeriodFrom(year, month);
-    const newTransaction = await transactionService.create({
+    const yearMonth = `${year}-${month.toString().padStart(2, '0')}`;
+    const yearMonthDay = `${yearMonth}-${day.toString().padStart(2, '0')}`;
+
+    const postTransaction = {
       description,
       value,
       category,
       year,
       month,
       day,
-      yearMonth: period,
-      yearMonthDay: dateHelpers.createPeriodFrom(year, month, day),
+      yearMonth,
+      yearMonthDay,
       type,
-    });
-    res.send({ status: 'Ok!', transaction: newTransaction });
-  } catch ({ mess }) {
-    console.log(mess);
-    res.status(400).send({ error: mess });
+    };
+
+    const newTransaction = await transactionService.postTransactions(
+      postTransaction
+    );
+
+    res.send({ status: 'Ok', transaction: newTransaction });
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
   }
 });
+
+transactionRouter.put('/', async (req, res) => {
+  try {
+    throw new Error('Conteúdo inexistente');
+
+    //Mongo DB
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
+  }
+});
+
 transactionRouter.put('/:id', async (req, res) => {
   const { body, params } = req;
 
+  console.log(params);
+
   try {
-    await validateTransactionId(params);
-    await validateTransactionData(body);
+    if (JSON.stringify(body) === '{}') {
+      throw new Error('Conteúdo inexistente');
+    }
 
     const { description, value, category, year, month, day, type } = body;
     const { id } = params;
-    const period = dateHelpers.createPeriodFrom(year, month);
+    const yearMonth = `${year}-${month.toString().padStart(2, '0')}`;
+    const yearMonthDay = `${yearMonth}-${day.toString().padStart(2, '0')}`;
 
-    const newTransaction = await transactionService.update(id, {
+    const updateTransaction = {
       description,
       value,
       category,
       year,
       month,
       day,
-      yearMonth: period,
-      yearMonthDay: dateHelpers.createPeriodFrom(year, month, day),
+      yearMonth,
+      yearMonthDay,
       type,
-    });
-    res.send({ status: 'Ok!', transaction: newTransaction });
-  } catch ({ mess }) {
-    console.log(mess);
-    res.status(400).send({ error: mess });
+    };
+
+    const updatedTransaction = await transactionService.updateTransaction(
+      id,
+      updateTransaction
+    );
+
+    res.send({ status: 'Ok', transaction: updatedTransaction });
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
+  }
+});
+
+transactionRouter.delete('/', async (req, res) => {
+  try {
+    throw new Error('Conteúdo inexistente');
+
+    //Mongo DB
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
   }
 });
 
 transactionRouter.delete('/:id', async (req, res) => {
   const { params } = req;
 
+  console.log(params);
+
   try {
-    await validateTransactionId(params);
     const { id } = params;
 
-    await transactionService.remove(id);
-    res.send({
-      status: 'Ok!',
-      message: `Lançamento com id (${id} excluído com sucesso!)`,
-    });
-  } catch ({ mess }) {
-    console.log(mess);
-    res.status(400).send({ error: mess });
+    const deleted = await transactionService.deleteTransaction(id);
+
+    if (deleted) {
+      res.send({ status: 'Ok', message: `${id} excluído com sucesso!` });
+    } else {
+      throw new Error('Não foi possível excluir');
+    }
+  } catch ({ message }) {
+    console.log(message);
+    res.status(400).send({ error: message });
   }
 });
 
--- services/transactionService.js
+++ services/transactionService.js
@@ -1,114 +1,31 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
-function clearTransaction(mongoDbDate) {
-  const {
-    _id,
-    description,
-    value,
-    category,
-    year,
-    month,
-    day,
-    yearMonth,
-    yearMonthDay,
-    Type,
-  } = mongoDbDate;
-
-  const reTransaction = {
-    _id,
-    description,
-    value,
-    category,
-    year,
-    month,
-    day,
-    yearMonth,
-    yearMonthDay,
-    Type,
-  };
-  return reTransaction;
-}
-
-// Aqui havia um erro difícil de pegar. Importei como "transactionModel",
-// com "t" minúsculo. No Windows, isso não faz diferença. Mas como no Heroku
-// o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
-// descobrir esse erro :-/
 const TransactionModel = require('../models/TransactionModel');
 
-//Busca os dados no banco, por período e descrição
-async function find(req, res) {
-  try {
-    let data;
-    const { period, description } = req.query;
-    if (!description) {
-      data = await getData(period);
-      console.log(`GET: Objects(${period ? '?period=' + period : ''}) `);
-    }
-    if (period && description) {
-      data = await getDataByDescription(description, period);
-      console.log(`GET: Object (?period=${period}&description=${description})`);
-    }
-    res.send(data);
-  } catch (err) {
-    res.status(!!err.errNumber ? err.errNumber : 400).send(err.message);
-  }
+async function getTransactionsFrom(period) {
+  const transactions = await TransactionModel.find({ yearMonth: period });
+  return transactions;
 }
 
-async function getDataByDescription(description = '', filter) {
-  //Filtro descrição por período
-  const desFilter = /^[12]\d{3}-(0[1-9]|1[012])$/;
-  if (!filter.match(desFilter)) {
-    throw { message: 'Informar perído no formato AAAA-MM' };
-  }
-  if (!description) {
-    throw { message: 'Descrição vazia', errNumber: 406 };
-  }
-  const regEx = new RegExp(description, 'i');
-  return TransactionModel.find({ yearMonth: filter, description: regEx }).sort({
-    yearMonthDay: 'asc',
-  });
-}
-// Função busca por período (data)
-function getData(filter) {
-  const regEx = /^[12]\d{3}-(0[1-9]|1[012])$/;
-  if (filter) {
-    if (!filter.match(regEx)) {
-      throw { message: 'Informar período no formato AAAA-MM' };
-    }
-    return TransactionModel.find({ yearMonth: filter }).sort({
-      yearMonth: 'asc',
-    });
-  }
-  return TransactionModel.find();
-}
-//http://localhost:3001/api/transaction?2019-01
-//(localiza todas as trasções em janeiro de 2019)
-async function findDistincPeriods(_, res) {
-  try {
-    const data = await TransactionModel.distinct('yearMonth');
-    console.log(`GET: Objects (.distinct('yearMonth'))`);
-    res.send(data);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
+async function postTransactions(transaction) {
+  const newTransaction = await TransactionModel.create(transaction);
+  return newTransaction;
 }
 
-async function create(transaction) {
-  const newDateMongoDB = await TransactionModel.create(transaction);
-  const newDate = clearTransaction(newDateMongoDB);
-  return newDate;
-}
-
-async function update(_id, transaction) {
-  await TransactionModel.updateOne({ _id: ObjectId(_id), transaction });
+async function updateTransaction(_id, transaction) {
+  await TransactionModel.updateOne({ _id: ObjectId(_id) }, transaction);
   return { _id, ...transaction };
 }
 
-//http://localhost:3001/api/transaction/id:5f173dc3822799348c486b54
-async function remove(_id) {
-  await TransactionModel.deleteOne({ _id: ObjectId(_id) });
-  return true;
+async function deleteTransaction(_id) {
+  const result = await TransactionModel.deleteOne({ _id: ObjectId(_id) });
+  return result.deletedCount === 1;
 }
 
-module.exports = { find, findDistincPeriods, create, update, remove };
+module.exports = {
+  getTransactionsFrom,
+  postTransactions,
+  updateTransaction,
+  deleteTransaction,
+};
